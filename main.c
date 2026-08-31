ce2c160f64b0b5f0d95c48d4705aeeab72a3e1b8
marcos <e-marcosap@weg.net>
2026-08-31 10:01:54 -0300

    read_file_whole

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include "base.h"
-
 #include <stdio.h>
+#include "lang.h"
+
+extern void* calloc(size_t, size_t);
+
+String read_file_whole(char const* path){
+	FILE* f = fopen(path, "rb");
+	fseek(f, 0, SEEK_END);
+	i64 end = ftell(f);
+	rewind(f);
+	i64 start = ftell(f);
+	i64 size = end - start;
+	u8* data = calloc(size + 1, 1);
+	fread(data, 1, size, f);
+
+	return (String){ .v = (char const*)data, .len = size };
+}
 
 int main(){
-	printf("yo");
+	String source = read_file_whole("main.c");
+	printf("%.*s\n", strf(source));
 }
 
 #include "base.c"
