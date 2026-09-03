993028b9402436065763bb2c3ea4e5973a24c8b2
marcos <e-marcosap@weg.net>
2026-09-03 15:41:26 -0300

    Round hood hashing

--- hash_table.h
+++ hash_table.h
@@ -86,26 +86,50 @@ void ht_init(Hash_Table* tbl, u32 cap, Arena* arena){
 }
 
 static inline
-Hash_Table_Slot* ht_find(Hash_Table* tbl, u32 hash, String key){
-	if(tbl->slot_count == 0){ return NULL; }
+u32 ht_probe_distance(Hash_Table const* tbl, u32 pos, u32 hash){
+	u32 mask = tbl->slot_count - 1;
+	u32 home = hash & mask;
+	return (pos - home) & mask;
+}
+
+static inline
+Hash_Table_Slot* ht_find(Hash_Table const* tbl, u32 hash, String key){
+	if(tbl->slot_count == 0){
+		return NULL;
+	}
 
 	u32 mask = tbl->slot_count - 1;
 	u32 pos = hash & mask;
+	u32 probe_dist = 0;
 
-	for(u32 probe = 0; probe < tbl->slot_count; probe += 1){
+	for(;;){
 		Hash_Table_Slot* slot = &tbl->slots[pos];
+
 		if(slot->hash == 0){
 			return NULL;
 		}
 
-		if((hash == slot->hash) && str_equal(key, slot->key)){
+		u32 slot_dist = ht_probe_distance(tbl, pos, slot->hash);
+
+		// Resident has travelled less than we have, so our key cannot appear after this point.
+		if(slot_dist < probe_dist){
+			return NULL;
+		}
+
+		if((slot->hash == hash) && str_equal(slot->key, key)){
 			return slot;
 		}
 
 		pos = (pos + 1) & mask;
+		probe_dist += 1;
 	}
+}
 
-	return NULL;
+static inline
+f32* ht_get(Hash_Table const* tbl, String key){
+	u32 hash = ht_key_hash(key);
+	Hash_Table_Slot* slot = ht_find(tbl, hash, key);
+	return &slot->value;
 }
 
 static inline
@@ -116,38 +140,87 @@ bool ht_needs_growth(Hash_Table const* tbl){
 
 static inline
 void ht_put(Hash_Table* tbl, String key, f32 value){
-	if(ht_needs_growth(tbl)){ // TODO: Defer rezising to after we add new key, here just cehck minimum vacancy
+	if(ht_needs_growth(tbl)){
 		panic("todo: growth");
 	}
 
 	u32 mask = tbl->slot_count - 1;
-	u32 hash = ht_key_hash(key);
-	u32 first_pos = hash & mask;
 
-	u32 pos = first_pos;
-	do {
+	Hash_Table_Slot incoming = {
+		.key   = key,
+		.value = value,
+		.hash  = ht_key_hash(key),
+	};
+
+	u32 pos = incoming.hash & mask;
+	u32 incoming_dist = 0;
+
+	for(;;){
 		Hash_Table_Slot* slot = &tbl->slots[pos];
 
 		if(slot->hash == 0){
-			// Vacant slot, place key
-			slot->key = key;
-			slot->value = value;
-			slot->hash = hash;
+			*slot = incoming;
 			tbl->in_use += 1;
 			return;
 		}
 
-		if((hash == slot->hash) && str_equal(key, slot->key)){
-			slot->value = value;
+		if((slot->hash == incoming.hash) && str_equal(slot->key, incoming.key)){
+			slot->value = incoming.value;
 			return;
 		}
 
+		u32 slot_dist = ht_probe_distance(tbl, pos, slot->hash);
+
+		if(slot_dist < incoming_dist){
+			Hash_Table_Slot tmp = *slot;
+			*slot = incoming;
+			incoming = tmp;
+
+			incoming_dist = slot_dist;
+		}
+
 		pos = (pos + 1) & mask;
-
-	} while(pos != first_pos);
+		incoming_dist += 1;
+	}
 }
-
 
 static inline
-void ht_remove(Hash_Table* tbl, String key){
+bool ht_remove(Hash_Table* tbl, String key){
+	u32 hash = ht_key_hash(key);
+	Hash_Table_Slot* found = ht_find(tbl, hash, key);
+	if(found == NULL){
+		return false;
+	}
+
+	u32 mask = tbl->slot_count - 1;
+	u32 hole = (u32)(found - tbl->slots);
+	u32 next = (hole + 1) & mask;
+
+	for(;;){
+		Hash_Table_Slot* slot = &tbl->slots[next];
+
+		// Actual end of cluster.
+		if(slot->hash == 0){
+			break;
+		}
+
+		u32 dist = ht_probe_distance(tbl, next, slot->hash);
+
+		// This entry is already at home. It doesn't depend on the
+		// hole, and neither does the following Robin Hood run.
+		if(dist == 0){
+			break;
+		}
+
+		// Collapse the displaced run backward by one.
+		tbl->slots[hole] = *slot;
+
+		hole = next;
+		next = (next + 1) & mask;
+	}
+
+	mem_zero(&tbl->slots[hole], sizeof(tbl->slots[hole]));
+	tbl->in_use -= 1;
+	return true;
 }
+
